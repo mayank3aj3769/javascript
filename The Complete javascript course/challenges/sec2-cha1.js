c73491a99d8a39aeb53f4099fc71b15e61014cbc
Mayank Raj <mayank.raj3769@gmail.com>
2021-06-12 13:10:43 +0530

    The complete javascript course challenges:

---
+++ The Complete javascript course/challenges/sec2-cha1.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const readline=require('readline-sync');
+var Dolphins = [];
+var Koalas=[];
+var size = 3; //Maximum Array size
+console.log("Enter dolphins score");
+for(var i=0; i<size; i++) {
+	
+	//Taking Input from user
+	Dolphins[i] = Number(readline.question('Enter score ' + (i+1)+' '));
+}
+console.log("Enter koala score");
+
+for(var i=0; i<size; i++) {
+	
+	//Taking Input from user
+	Koalas[i] = Number(readline.question('Enter score ' + (i+1)+' '));
+}
+
+
+const calcAverage=(arr )=>{
+    let avg;
+    for(let i=1;i<=3;i++){
+        avg+=arr[i];
+    }
+    avg/=3;
+}
+
+const checkWinner=(avgDolphins,avgKoalas)=>{
+    if (avgDolphins===avgKoalas*2)
+    {
+        console.log(`Dolhphins win (${avgDolphins} vs. ${avgKoalas})`);
+    }
+    else if(avgKoalas===avgDolphins*2)
+    {
+        console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+    }
+        
+    else
+    console.log("No result");
+}
+const avgDol=calcAverage(...Dolphins);
+const avgKol=calcAverage(...Koalas);
+
+checkWinner(avgDol,avgKol);
